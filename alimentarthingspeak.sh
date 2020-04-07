d3b333a96af3c25c9d98eb782cb256feeb0a9ddd
jarteaga77 <arteaga_jonathan@hotmail.comgit commit -m tercer>
2020-04-07 22:32:51 +0000

    tercer commit

---
+++ alimentarthingspeak.sh
@@ -0,0 +1,11 @@
+
+#!/usr/bin/env bash
+KEYWRITE=SB2ABPCDMF5O8O9Q
+INSTALL_DIR=$(pwd)
+DISCO=$(${INSTALL_DIR}/disco.sh)
+MEMORIA=$(${INSTALL_DIR}/memoria.sh)
+LOADAVG1=$(${INSTALL_DIR}/loadavg.sh)
+LOADAVG5=$(${INSTALL_DIR}/loadavg.sh 5)
+LOADAVG15=$(${INSTALL_DIR}/loadavg.sh 15)
+curl https://api.thingspeak.com/update?api_key=${KEYWRITE}\&field1=${MEMORIA}\&field2=${LOADAVG1}\&field3=${LOADAVG5}\&field4=${LOADAVG15}\&field5=${DISCO}
+
